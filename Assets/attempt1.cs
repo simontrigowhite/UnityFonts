bb3109cf882b1b22780909d350ed400e157922a4
Simon <simontrigowhite@gmail.com>
2017-12-30 22:19:41 +0000

    add importer code
    from http://www.benoitfreslon.com/2015/03/unity-generate-and-import-a-bitmap-font-with-a-free-tool/

--- Assets/attempt1.cs
+++ Assets/attempt1.cs
@@ -15,6 +15,8 @@ public class attempt1 : MonoBehaviour {
         Debug.Log("hello");
         pointless();
         Import();
+
+        //BitmapFontImporter.GenerateFont();
 	}
 
     void pointless()
@@ -28,36 +30,36 @@ public class attempt1 : MonoBehaviour {
         Debug.Log(rects.text); 
         Debug.Log(rects.font.name); 
 
-        Vector2 size = new Vector2(2F, 6F);
-        Font font = new Font();
-        var _splitFile = new string[] { "\r\n", "\r", "\n" };
-        var _splitLine = new char[] { ',' };
+        //Vector2 size = new Vector2(2F, 6F);
+        //Font font = new Font();
+        //var _splitFile = new string[] { "\r\n", "\r", "\n" };
+        //var _splitLine = new char[] { ',' };
 
-        string[] lines = rects.text.Split(_splitFile, StringSplitOptions.None);
+        //string[] lines = rects.text.Split(_splitFile, StringSplitOptions.None);
 
-        CharacterInfo[] info = new CharacterInfo[lines.Length];
+        //CharacterInfo[] info = new CharacterInfo[lines.Length];
 
-        for (var i = 0; i < lines.Length; i++)
-        {
-            string[] line = lines[i].Split(_splitLine, StringSplitOptions.None);
-            int x = Convert.ToInt32(line[0]);
-            int y = Convert.ToInt32(line[1]);
-            int width = Convert.ToInt32(line[2]);
-            int height = Convert.ToInt32(line[3]);
-            int offset = Convert.ToInt32(line[4]);
-            /*info[i].uv.x = x / size.x;
-            info[i].uv.y = y / size.y;
-            info[i].uv.width = width / size.x;
-            info[i].uv.height = height / size.y;
-            info[i].minX = 0;info[i].vert.x = 0;
-            info[i].minY = -offset;info[i].vert.y = -offset;
-            info[i].maxX = width;info[i].vert.width = width;
-            info[i].maxY = -height;info[i].vert.height = -height;
-            info[i].advance = width;info[i].width = width;*/
-            info[i].index = i;
-        }
-        font.characterInfo = info;
-        AssetDatabase.SaveAssets();
+        //for (var i = 0; i < lines.Length; i++)
+        //{
+        //    string[] line = lines[i].Split(_splitLine, StringSplitOptions.None);
+        //    int x = Convert.ToInt32(line[0]);
+        //    int y = Convert.ToInt32(line[1]);
+        //    int width = Convert.ToInt32(line[2]);
+        //    int height = Convert.ToInt32(line[3]);
+        //    int offset = Convert.ToInt32(line[4]);
+        //    /*info[i].uv.x = x / size.x;
+        //    info[i].uv.y = y / size.y;
+        //    info[i].uv.width = width / size.x;
+        //    info[i].uv.height = height / size.y;
+        //    info[i].minX = 0;info[i].vert.x = 0;
+        //    info[i].minY = -offset;info[i].vert.y = -offset;
+        //    info[i].maxX = width;info[i].vert.width = width;
+        //    info[i].maxY = -height;info[i].vert.height = -height;
+        //    info[i].advance = width;info[i].width = width;*/
+        //    info[i].index = i;
+        //}
+        //font.characterInfo = info;
+        //AssetDatabase.SaveAssets();
     }
 	
 	// Update is called once per frame
