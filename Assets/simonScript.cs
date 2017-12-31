842a63c863f32622f7803a7c930c99b76a40c7aa
Simon <simontrigowhite@gmail.com>
2017-12-31 00:25:11 +0000

    add script and ref to text object

---
+++ Assets/simonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Simon();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    void Simon()
+    {
+        UnityEngine.UI.Text textAsset = this.GetComponent<UnityEngine.UI.Text>();
+        //var x =this.GetComponent(text)
+        Debug.Log(textAsset.name);
+    }
+
+}
